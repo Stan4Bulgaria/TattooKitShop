a3a2bb1da00cb8990416647f4518331b9997579c
Stanislav Lazov <stanislav_lazov1@abv.bg>
2025-07-08 01:15:45 +0300

    Adjusting user secrets and program.cs

--- TattooKitShop/Program.cs
+++ TattooKitShop/Program.cs
@@ -31,9 +31,7 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapDefaultControllerRoute();
 app.MapRazorPages();
 
-app.Run();
+await app.RunAsync();
