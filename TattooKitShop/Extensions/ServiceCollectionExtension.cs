ea50ddd1d48435ecd8f075ca55b811ec372c5330
Stanislav Lazov <stanislav_lazov1@abv.bg>
2025-07-07 23:55:48 +0300

    Creating the project and adding the Extension class

---
+++ TattooKitShop/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,19 @@
+﻿namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection service)
+        {
+
+            return service;
+        }
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection service, IConfiguration config)
+        {
+            return service;
+        }
+        public static IServiceCollection AddApplicationIndentity(this IServiceCollection service, IConfiguration config)
+        {
+            return service;
+        }
+    }
+}
