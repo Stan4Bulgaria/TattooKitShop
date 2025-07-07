e75ff985e4c5d1213312b23108c8dfe6e84fc4bf
Stanislav Lazov <stanislav_lazov1@abv.bg>
2025-07-08 00:15:13 +0300

    Adding the controllers

---
+++ TattooKitShop/Controllers/PaymentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TattooKitShop.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
